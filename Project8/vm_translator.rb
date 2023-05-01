d32107d8aa6497607c5b9cc7421940ddc9988c35
EleoraSasson <eleora@g.jct.ac.il>
2023-05-01 14:36:21 +0300

    fixed project 7

--- Project8/vm_translator.rb
+++ Project8/vm_translator.rb
@@ -5,6 +5,7 @@ class VMTranslator
 
   puts("Enter the path to a vm file, or to a directory containing vm files")
   path = gets.chomp
+  path = "C:\\Users\\eleor\\OneDrive\\Bureau\\Year 4\\Semester 2\\Fundamentals\\Homework 1\\Project7_lab1\\StackArithmetic\\StackTest\\StackTest.vm"
 
   #if the path ends by .vm, then create a instance of codewriter and call create_output
   #if path.end_with?(".vm")
