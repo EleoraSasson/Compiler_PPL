8666c8051f46daa68a066a0f5ee5c1e09fb29c44
EleoraSasson <eleora@g.jct.ac.il>
2023-05-03 14:52:30 +0300

    works for a directory containing many vm files

--- Project8/vm_translator.rb
+++ Project8/vm_translator.rb
@@ -4,8 +4,8 @@ require_relative 'code_writer'
 class VMTranslator
 
   puts("Enter the path to a vm file, or to a directory containing vm files")
-  path = gets.chomp
-  #path = 'C:\Users\eleor\OneDrive\Bureau\Year 4\Semester 2\Fundamentals\nand2tetris\nand2tetris\projects\08\FunctionCalls\FibonacciElement\Sys.vm'
+  #path = gets.chomp
+  path = 'C:\Users\eleor\OneDrive\Bureau\Year 4\Semester 2\Fundamentals\Homework 1\Project7_lab1\vm files'
   #path = path.gsub(/\0/, '')
   if path.start_with?('"') && path.end_with?('"')
     # remove the double quotes from the input string using gsub and a regular expression
@@ -16,12 +16,19 @@ class VMTranslator
     code_writer = CodeWriter.new(path)
     code_writer.create_output
   else
+    #TODO: if vm files need to be translated in only one asm, how do we do it?
     # If path is not a .vm file, assume it is a directory and search for .vm files in it
-    vm_files = Dir.glob(File.join(path, "*.vm"))
-    if vm_files.empty?
+    files = Dir.entries(path)
+    result = []
+    if files.empty?
       puts "No .vm files found in directory #{path}"
     else
-      vm_files.each do |vm_file|
+      files.each do |f|
+        if f.end_with?(".vm") && File.file?(File.join(path, f))
+          result << File.join(path, f)
+        end
+      end
+      result.each do |vm_file|
         code_writer = CodeWriter.new(vm_file)
         code_writer.create_output
       end
