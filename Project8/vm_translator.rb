c87c237596b304006a1b1b48b970b18cbf5c54d7
EleoraSasson <eleora@g.jct.ac.il>
2023-05-03 14:11:58 +0300

    .

--- Project8/vm_translator.rb
+++ Project8/vm_translator.rb
@@ -4,9 +4,9 @@ require_relative 'code_writer'
 class VMTranslator
 
   puts("Enter the path to a vm file, or to a directory containing vm files")
-  #path = gets.chomp
-  path = 'C:\Users\eleor\OneDrive\Bureau\Year 4\Semester 2\Fundamentals\nand2tetris\nand2tetris\projects\08\FunctionCalls\FibonacciElement\Sys.vm'
-  path = path.gsub(/\0/, '')
+  path = gets.chomp
+  #path = 'C:\Users\eleor\OneDrive\Bureau\Year 4\Semester 2\Fundamentals\nand2tetris\nand2tetris\projects\08\FunctionCalls\FibonacciElement\Sys.vm'
+  #path = path.gsub(/\0/, '')
   if path.start_with?('"') && path.end_with?('"')
     # remove the double quotes from the input string using gsub and a regular expression
     path = path.gsub(/^"|"$/, '')
@@ -15,13 +15,6 @@ class VMTranslator
   if path.end_with?(".vm")
     code_writer = CodeWriter.new(path)
     code_writer.create_output
-  end
-
-=begin
-  elsif path.end_with?(".vm")
-    code_writer = CodeWriter.new(path)
-    code_writer.create_output
-=begin
   else
     # If path is not a .vm file, assume it is a directory and search for .vm files in it
     vm_files = Dir.glob(File.join(path, "*.vm"))
@@ -32,7 +25,6 @@ class VMTranslator
         code_writer = CodeWriter.new(vm_file)
         code_writer.create_output
       end
-=end
-    #end
-  #end
+      end
+  end
 end
