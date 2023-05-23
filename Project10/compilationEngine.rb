4b01818a81d76d39c329b912e20cf366f818235f
meira grafstein <grafstei@g.jct.ac.il>
2023-05-23 14:58:20 +0300

    added class compilation engine

---
+++ Project10/compilationEngine.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require_relative 'tokenizer'
+class CompilationEngine
+
+  def initialize
+
+  end
+
+end
--- Project10/main.rb
+++ Project10/main.rb
@@ -3,7 +3,8 @@ require_relative 'tokenizer'
 class Main
 
   puts("Please enter a jack file or a directory containing a jack file")
-  path = 'C:\Users\eleor\OneDrive\Bureau\Year 4\Semester 2\Fundamentals\nand2tetris\nand2tetris\projects\10\ArrayTest\Main.jack'
+  #path = 'C:\Users\eleor\OneDrive\Bureau\Year 4\Semester 2\Fundamentals\nand2tetris\nand2tetris\projects\10\ArrayTest\Main.jack'
+  path = gets.chomp
   if path.start_with?('"') && path.end_with?('"')
     # remove the double quotes from the input string using gsub and a regular expression
     path = path.gsub(/^"|"$/, '')
